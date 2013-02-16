59b2205825a310225142323ae735db66a63637e7
Lucas Souza <lucasfs7@gmail.com>
2013-02-16 02:36:29 -0200

    loading all pages from once on page load (just for not paginate it now - works fine with a few pictures)

--- get_images.php
+++ get_images.php
@@ -1,11 +1,24 @@
 <?php
 header('Content-type: application/json');
-$url = "https://api.instagram.com/v1/tags/snow/media/recent?access_token=6342485.f59def8.ccbe5efec33b42ae81047565a0dec26b";
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+$data = array();
 
-$result = json_decode(curl_exec($ch));
-echo json_encode($result->data);
+function get_images($url) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+  $result = json_decode(curl_exec($ch));
+
+  foreach ($result->data as $d) {
+    $data[] = $d;
+  }
+
+  if (isset($result->pagination->next_url)) {
+    get_images($result->pagination->next_url);
+  } else {
+    echo json_encode($data);
+  }
+}
+
+get_images("https://api.instagram.com/v1/tags/OdeAosNoivos/media/recent?access_token=6342485.f59def8.ccbe5efec33b42ae81047565a0dec26b");
